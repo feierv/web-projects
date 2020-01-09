02e17d931b36b173afee30b7be70daec6439ed1a
feierv <49754373+feierv@users.noreply.github.com>
2020-01-09 22:48:56 +0200

    Delete connection.php

--- chatApp/proiect/proiect/connection.php
+++
@@ -1,6 +0,0 @@
- <?php $link =  mysqli_connect("shareddb-r.hosting.stackcp.net","chatUsers-313233295f","chatUsers2019","chatUsers-313233295f"); 
-        if( mysqli_connect_error()){
-
-                                 die("there was an error connecting to the database!");
-                              } 
-        ?>
